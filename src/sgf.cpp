c3ece98cb7c93292a0a762a7ee0114644fbc07e6
fishku <isaacbd@gmail.com>
2024-06-21 00:26:17 +0200

    Handle handicap stones

--- src/sgf.cpp
+++ src/sgf.cpp
@@ -40,21 +40,41 @@ void load_sgf(const std::string& file_path, Board& board, std::vector<Move>& mov
 
     board = Board(Vec2{size_x, size_y}, komi);
 
-    // Extract moves
+    // Extract moves and handicap stones
     // Doesn't handle branches!!
-    const std::regex move_regex(R"([BW]\[([a-z]{2})?\])");
+    const std::regex move_regex(R"(([BW])\[([a-z]{2})?\]|A([BW])(\[[a-z]{2}\])+)");
+    const std::regex coord_regex(R"(\[([a-z]{2})\])");
     std::sregex_iterator move_iter(content.begin(), content.end(), move_regex);
-    const std::sregex_iterator move_end;
-    while (move_iter != move_end) {
+    const std::sregex_iterator end;
+    while (move_iter != end) {
         const std::smatch move_match = *move_iter;
-        const Color color = move_match[0].str()[0] == 'B' ? Black : White;
+
         if (move_match[1].matched) {
-            const char col = move_match[1].str()[0] - 'a';
-            const char row = move_match[1].str()[1] - 'a';
-            moves.push_back({color, {static_cast<int>(col), static_cast<int>(row)}});
-        } else {
-            moves.push_back(pass);
+            // Regular move
+            const Color color = move_match[1].str()[0] == 'B' ? Black : White;
+            if (move_match[2].matched) {
+                const char col = move_match[2].str()[0] - 'a';
+                const char row = move_match[2].str()[1] - 'a';
+                moves.push_back({color, {static_cast<int>(col), static_cast<int>(row)}});
+            } else {
+                moves.push_back(pass);
+            }
+        } else if (move_match[3].matched) {
+            // Handicap stones (AB or AW)
+            const Color color = move_match[3].str()[0] == 'B' ? Black : White;
+            const std::string coords_str = move_match[0].str();
+
+            std::sregex_iterator coord_iter(coords_str.begin(), coords_str.end(), coord_regex);
+
+            while (coord_iter != end) {
+                const std::smatch coord_match = *coord_iter;
+                const char col = coord_match[1].str()[0] - 'a';
+                const char row = coord_match[1].str()[1] - 'a';
+                moves.push_back({color, {static_cast<int>(col), static_cast<int>(row)}});
+                ++coord_iter;
+            }
         }
+
         ++move_iter;
     }
 }
